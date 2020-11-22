d7b16a3984417d788afb33a941f706f43f7e5313
Jason Williams <smailliwnosaj@yahoo.com>
2020-11-22 14:43:33 -0500

    Add files via upload

---
+++ ConsoleApp/Models/Coordinate.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ConsoleApp1.Models
+{
+    public class Coordinate
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
---
+++ ConsoleApp/Models/PotentialSolutions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1.Models
+{
+    public class PotentialSolutions
+    {
+        public Coordinate Coordinate { get; set; }
+        public List<int> PossibleValues { get; set; }
+    }
+}
