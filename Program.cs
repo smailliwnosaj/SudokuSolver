c939fae866c033a25f510bf59e446c90dfe2f465
Jason Williams <smailliwnosaj@yahoo.com>
2020-11-22 14:38:08 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,208 @@
+﻿using ConsoleApp.Models;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            // SUDOKU SOLVER 
+            // Solve any Sudoku puzzle....even the hardest puzzles in the world!
+            // If a puzzle has multiple solutions, this will show every possible solution.
+
+            // INSTRUCTIONS:
+            // Puzzles are defined, below
+            // To try a different puzzle, change the code...you should know how...don't make me teach you that part.
+            // Debug the code.
+            // You will see a console application start running.
+
+            // START OF SUDOKU PUZZLE SAMPLES
+
+            // Typical "Easy" puzzle
+            // 38 provided numbers - 1 possible outcome - 5 solution - max hypothesis of 2
+            //var puzzle = new string[9] {
+            //    "9, ,8, , ,4, ,6,2",
+            //    "3,1,2,6, , ,4, , ",
+            //    "6,7,4, ,1, , ,3,8",
+            //    "7, ,1, , , , , , ",
+            //    " , , ,8,5, ,7, ,6",
+            //    " ,2, , ,7,3,8,9, ",
+            //    "8,6,3, ,9,7, ,5, ",
+            //    " ,9,5, ,4,2, , , ",
+            //    " , , , , , ,9, , "
+            //};
+
+            // HARDEST PROBLEM IN THE WORLD!!!!  Though, if you find a more difficult problem, please let me know.
+            // http://www.free-sudoku.com/sudoku.php?id=127732
+            // 17 provided numbers - 1748499 possible outcomes - 1 solution - max hypothesis of 2
+            // Depending on how good your development system is, this solution could take +/- 40 minutes of (single thread) processing and a good deal of system memory.
+            // This puzzle requires making hypothetical guesses about 800k times.
+            // For each hypothetical guess, the code will split the puzzle into multiple puzzles.
+            // The code will solve each of 1748499 puzzles until it finds the one unique solution.
+            //var puzzle = new string[9] {
+            //    " ,5, , ,1, , , , ",
+            //    " , , , , , ,4,6, ",
+            //    " , , , , , ,3, ,8",
+            //    "6, , ,3, , , , , ",
+            //    " ,7, , , , , ,1, ",
+            //    " , , , , ,4, , , ",
+            //    " , , ,5,7, ,2, , ",
+            //    "3, ,4, , , , , , ",
+            //    " , , ,1, , , , , "
+            //};
+
+            //var puzzle = new string[9] {
+            //    " , , , , , , , , ",
+            //    " , , , , , , , , ",
+            //    " , , , , , , , , ",
+            //    " , , , , , , , , ",
+            //    " , , , , , , , , ",
+            //    " , , , , , , , , ",
+            //    " , , , , , , , , ",
+            //    " , , , , , , , , ",
+            //    " , , , , , , , , "
+            //};
+
+            //var puzzle = new string[9] {
+            //    " ,2,9,1, , , ,6, ",
+            //    " ,7, , , , , , ,5",
+            //    " , ,1,3, ,4, , , ",
+            //    " , ,3, , , , , ,9",
+            //    " , , ,6,4, ,8, , ",
+            //    " , , ,5,3, , , ,2",
+            //    " ,9, , , , ,2, , ",
+            //    " , , ,2, , , , , ",
+            //    "8, , , , , , ,5,7"
+            //};
+
+            // Finnish mathematician Arto Inkala devised this Sudoku puzzle in 2012, dubbing it the most 
+            // difficult setup of the puzzle in the world. He named the thing Everest, because, well, 
+            // conquering it is an incomprehensible feat.Inkala specifically designed Everest to be unsolvable 
+            // to anyone but the most brilliant.For reference, most Sudoku grids are graded on a five - star scale, 
+            // where five stars denotes the most challenging puzzles. According to this ranking system, 
+            // Inkala rated Everest an eleven
+            // 21 provided numbers - 69853 possible outcomes - 1 solution - max hypothesis of 2
+            var puzzle = new string[9] {
+                "8, , , , , , , , ",
+                " , ,3,6, , , , , ",
+                " ,7, , ,9, ,2, , ",
+                " ,5, , , ,7, , , ",
+                " , , , ,4,5,7, , ",
+                " , , ,1, , , ,3, ",
+                " , ,1, , , , ,6,8",
+                " , ,8,5, , , ,1, ",
+                " ,9, , , , ,4, , "
+            };
+
+            // Arto Inkala has made what he claims is the hardest sudoku puzzle ever. According to the Finnish puzzle maker 
+            // "I called the puzzle AI Escargot, because it looks like a snail. Solving it is like an intellectual culinary pleasure." 
+            // If you're open for the challenge, AI Escargot presumably requires you to wrap your brain around eight casual 
+            // relationships simultaneously, whereas your everyday "very hard" sudoku piece, only require you to think about 
+            // a meager one or two of these relationships at once.
+            // 24 provided numbers - 2795 possible outcomes - 1 solution - max hypothesis of 2
+            //var puzzle = new string[9] {
+            //    "1, , , , ,7, ,9, ",
+            //    " ,3, , ,2, , , ,8",
+            //    " , ,9,6, , ,5, , ",
+            //    " , ,5,3, , ,9, , ",
+            //    " ,1, , ,8, , , ,2",
+            //    "6, , , , ,4, , , ",
+            //    "3, , , , , , ,1, ",
+            //    " ,4,1, , , , , ,7",
+            //    " , ,7, , , ,3, , "
+            //};
+
+            // The brainchild of Finnish mathematical whizz Arto Inkala, it took him three months to compile. 
+            // 23 provided numbers - 1009 possible outcomes - 1 solution - max hypothesis of 2
+            //var puzzle = new string[9] {
+            //    " , ,5,3, , , , , ",
+            //    "8, , , , , , ,2, ",
+            //    " ,7, , ,1, ,5, , ",
+            //    "4, , , , ,5,3, , ",
+            //    " ,1, , ,7, , , ,6",
+            //    " , ,3,2, , , ,8, ",
+            //    " ,6, ,5, , , , ,9",
+            //    " , ,4, , , , ,3, ",
+            //    " , , , , ,9,7, , "
+            //};
+            // I tested this problem with each number missing.  This version is the most difficult version of this puzzle.
+
+            // http://sw-amt.ws/sudoku/level-very-deep/zz-www.sudokuwiki.org-0177-base/zz-www.sudokuwiki.org-0177-base.html
+            // 23 provided numbers, 21685 possible outcomes - 1 solution - max hypothesis of 2
+            //var puzzle = new string[9] {
+            //    " , , , ,7,3, ,6, ",
+            //    " , , ,6, , ,4, ,9",
+            //    "6, , , , , ,3, , ",
+            //    "9, ,5, ,8, , , , ",
+            //    " ,1, , , , ,5, , ",
+            //    "3, , ,4, , , ,9, ",
+            //    " ,8, , , ,2, , , ",
+            //    "4, , ,7, , , ,3, ",
+            //    " , ,2, , , , , ,1"
+            //};
+
+            // http://sw-amt.ws/sudoku/level-very-deep/zz-www.sudokuwiki.org-0249-base/zz-www.sudokuwiki.org-0249-base.html
+            // 23 provided numbers - 100353 possible outcomes - 1 solution - max hypothesis of 2
+            //var puzzle = new string[9] {
+            //    " ,4, ,7, , ,1, , ",
+            //    " , , , ,2, , ,9, ",
+            //    "9, , , , , , , ,4",
+            //    " , ,5, , ,3, , , ",
+            //    "2, , , ,5, , , ,3",
+            //    " ,6, ,4, , ,8, , ",
+            //    " ,1, , , , , ,7,8",
+            //    " , , , , , ,4, , ",
+            //    " , ,4,8, , , ,6,1"
+            //};
+
+            // http://sw-amt.ws/sudoku/level-deep/xx-tarx0052-base/xx-tarx0052-base.html
+            // 21 provided numbers - 40799 possible outcomes - 1 solution - max hypothesis of 2
+            //var puzzle = new string[9] {
+            //    " , , , , , , , ,2",
+            //    " , ,8, , ,9,1, , ",
+            //    "5, , , , , , ,4, ",
+            //    " , , ,9, ,7, , , ",
+            //    " , ,7, ,3, ,8, , ",
+            //    " , , ,8, ,1, ,3, ",
+            //    " ,4, , ,6, , , ,5",
+            //    " , ,9,7, , ,3, , ",
+            //    "2, , , , , , , , "
+            //};
+
+            // https://www.free-sudoku.com/sudoku.php?dchoix=evil
+            // 17 provided numbers - 330543 possible outcomes - 1 solution - max hypothesis of 2
+            //var puzzle = new string[9] {
+            //    " , , , ,9,3, , , ",
+            //    "2, , , , , ,9, , ",
+            //    "6, , , , , , , , ",
+            //    " , , ,4, , , ,6, ",
+            //    " ,3, , , , , , ,1",
+            //    " , , ,2, , , , , ",
+            //    " ,7,1, , ,5, , , ",
+            //    " , , , ,4, ,6,2, ",
+            //    " , , , , , ,8, , "
+            //};
+
+            // Created by Jason Williams - 2020-03-12
+            // 23 provided numbers - 2383 possible outcomes - 1 solution - max hypothesis of 2
+            //var puzzle = new string[9] {
+            //    " , ,2, , ,8, , , ",
+            //    " ,4, , , ,6, , ,2",
+            //    " , , ,1,7, ,8, , ",
+            //    "7, , , , , , , , ",
+            //    " ,8, , , , , ,5, ",
+            //    " , ,9, , , , , ,3",
+            //    " , ,4, ,3,7, ,9, ",
+            //    "3,5, , , , , ,7, ",
+            //    " , , ,4,8, ,6, , "
+            //};
+            // I tested this problem with each number missing.  This version is the most difficult version of this puzzle.
+
+            // Create an instance of our Sudoku class
+            var solver = new Models.SudokuSolver(puzzle);
+        }
+
+    }
+
+}
---
+++ SudokuSolver.cs
@@ -0,0 +1,287 @@
+﻿using SudokuSolver.ConsoleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp.Models
+{
+    public class SudokuSolver
+    {
+        #region Local Variables
+        private List<Puzzle> _Puzzles { get; set; }
+        private int _IterationCount { get; set; }
+        private int _PuzzleCount { get; set; }
+        private DateTime _StartTime { get; set; }
+        private DateTime _EndTime { get; set; }
+        private int _TotalHypothesis { get; set; }
+        #endregion
+
+        #region Constructors
+        protected SudokuSolver() { }
+
+        public SudokuSolver(string[] rowCSVstrings)
+        {
+            _StartTime = DateTime.UtcNow;
+            _EndTime = DateTime.UtcNow;
+            _TotalHypothesis = 0;
+
+            _Puzzles = new List<Puzzle> ();
+            var array = new int?[9][];
+
+            if (rowCSVstrings == null || rowCSVstrings.Length != 9)
+            {
+                throw new FormatException("Error: Sudoku.cs constructor's rowCSVstrings property must be a string array of 9 comma-seperated-value strings.  Each string must have 0-9 integers between 1 and 9.  Use empty string for numbers you do not know the value of.");
+            }
+
+            for (int x = 0; x < 9; x++)
+            {
+                int?[] row = new int?[9];
+
+                var rowItemsAsStrings = rowCSVstrings[x].Split(',');
+                if (rowItemsAsStrings.Length != 9)
+                {
+                    throw new FormatException("Error: Sudoku.cs constructor's rowCSVstrings property must be a string array of 9 comma-seperated-value strings.  Each string must have 0-9 integers between 1 and 9.  Use empty string for numbers you do not know the value of.");
+                }
+
+                for (int y = 0; y < 9; y++) // For each row
+                {
+                    int value;
+                    int.TryParse(rowItemsAsStrings[y].Trim(), out value);
+
+                    if (value >= 1 && value <= 9) // For each integer in each row
+                    {
+                        row[y] = value;
+                    }
+                    else
+                    {
+                        row[y] = null;
+                    }
+                }
+
+                array[x] = row;
+            }
+
+            var puzzle = new Puzzle() { Solved = false, CanSolve = true, Array = array };
+            _Puzzles.Add(puzzle);
+            _PuzzleCount++;
+
+            Console.WriteLine("This is the Sudoku we will try to solve:");
+            DisplaySudokuInConsole(puzzle);
+
+            SolveAllPuzzlesRecursively();
+
+            Console.WriteLine("\nSolutions will appear below:\n");
+            foreach (var p in _Puzzles.Where(x => x.Solved == true && x.CanSolve == true))
+            {
+                DisplaySudokuInConsole(p);
+            }
+
+            Console.WriteLine("Total hypothetical guesses required: " + _TotalHypothesis + ".");
+            Console.WriteLine("Total processing duration: " + (DateTime.UtcNow - _StartTime));
+
+            System.Threading.Thread.Sleep(10000000);
+        }
+        #endregion
+
+        #region Private methods
+        private void DisplaySudokuInConsole(Puzzle puzzle)
+        {
+            Console.WriteLine("\nPuzzle {Solved:" + puzzle.Solved + ", CanSolve: " + puzzle.CanSolve + "}");
+            for (int x = 0; x < 9; x++)
+            {
+                Console.WriteLine(
+                    " " + puzzle.Array[x][0].ToString() + " " +
+                    " " + puzzle.Array[x][1].ToString() + " " +
+                    " " + puzzle.Array[x][2].ToString() + " " +
+                    " " + puzzle.Array[x][3].ToString() + " " +
+                    " " + puzzle.Array[x][4].ToString() + " " +
+                    " " + puzzle.Array[x][5].ToString() + " " +
+                    " " + puzzle.Array[x][6].ToString() + " " +
+                    " " + puzzle.Array[x][7].ToString() + " " +
+                    " " + puzzle.Array[x][8].ToString() + "");
+            }
+            Console.WriteLine("\n");
+        }
+
+        private void SolveAllPuzzlesRecursively()
+        {
+            var hasUnsolvedPuzzles = true;
+            var found = true;
+            while (hasUnsolvedPuzzles == true && found == true)
+            {
+                found = false;
+                for (int i = 0; i < _PuzzleCount; i++)
+                {
+                    if (_Puzzles[i].Solved == false && _Puzzles[i].CanSolve == true)
+                    {
+                        found = true;
+                        SolveAllStepsOfSudokuRecursively(_Puzzles[i]);
+                    }
+                }
+                hasUnsolvedPuzzles = _Puzzles.Where(x => x.Solved == false && x.CanSolve == true).Count() > 0;
+            }
+        }
+
+        private void SolveAllStepsOfSudokuRecursively(Puzzle puzzle)
+        {
+            while (puzzle.Solved == false && puzzle.CanSolve == true)
+            {
+                _IterationCount++;
+                Console.WriteLine("Processing step #" + _IterationCount);
+                SolveSingleStepOfSudoku(puzzle);
+            }
+        }
+
+        private void SolveSingleStepOfSudoku(Puzzle puzzle)
+        {
+            var lowestHypothesisLevel = 10;
+            var solved = true;
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    var value = GetValueForCoordinate(puzzle, x, y);
+                    if (value == null)
+                    {
+                        var possibleValues = GetPossibleValuesForCoordinate(puzzle, x, y);
+                        var possibleValuesCount = possibleValues.Count;
+                        //if (possibleValuesCount == 0)
+                        //    continue;
+
+                        if (possibleValuesCount == 1)
+                        {
+                            SetValueForCoordinate(puzzle, x, y, possibleValues[0]);
+                            return;
+                        }
+                        solved = false;
+                        if (possibleValuesCount > 1)
+                        {
+                            if (possibleValuesCount < lowestHypothesisLevel) lowestHypothesisLevel = possibleValuesCount;
+                        }
+                    }
+                }
+            }
+            if (solved == true)
+            {
+                Console.WriteLine("\nPuzzle is solved!!!!");
+                puzzle.Solved = true;
+                DisplaySudokuInConsole(puzzle);
+                return;
+            }
+            puzzle.CanSolve = false;
+            if (lowestHypothesisLevel <= 4) // 4 should be an arbitrary number between 2 and 9 depending on how many hypothesis should be attempted.  (NOTE: I've never seen a puzzle requiring greater than 2)
+            {
+                SplitPuzzleForMultipleHypothesis(puzzle, lowestHypothesisLevel);
+                // We created multiple puzzles from the source puzzle.  Source puzzle must be marked as not solveable.
+                return;
+            }
+            //throw new Exception("Puzzle cannot be solved with the currently available logic.");
+        }
+
+        private void SplitPuzzleForMultipleHypothesis(Puzzle puzzle, int hypothesisLevel)
+        {
+            _TotalHypothesis = _TotalHypothesis + hypothesisLevel;
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    var value = GetValueForCoordinate(puzzle, x, y);
+                    if (value == null)
+                    {
+                        var possibleValues = GetPossibleValuesForCoordinate(puzzle, x, y);
+                        var possibleValuesCount = possibleValues.Count;
+                        if (possibleValuesCount > 0 && possibleValuesCount <= hypothesisLevel)
+                        {
+                            Console.WriteLine("\nHypothesis Required: " + possibleValuesCount + " puzzles will be created with each possible value.\n");
+                            for (int i = 0; i < hypothesisLevel; i++)
+                            {
+                                var newPuzzle = new Puzzle() {
+                                    Array = new int?[9][],
+                                    CanSolve = true,
+                                    Solved = false
+                                };
+                                for (int x1 = 0; x1 < 9; x1++)
+                                {
+                                    newPuzzle.Array[x1] = new int?[9];
+                                    for (int y1 = 0; y1 < 9; y1++)
+                                    {
+                                        newPuzzle.Array[x1][y1] = puzzle.Array[x1][y1];
+                                    }
+                                }
+                                SetValueForCoordinate(newPuzzle, x, y, possibleValues[i]);
+                                _Puzzles.Add(newPuzzle);
+                                _PuzzleCount++;
+                            }
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void SetValueForCoordinate(Puzzle puzzle, int x, int y, int? value)
+        {
+            puzzle.Array[x][y] = value;
+        }
+
+        private int? GetValueForCoordinate(Puzzle puzzle, int x, int y)
+        {
+            return puzzle.Array[x][y];
+        }
+
+        private List<int> GetPossibleValuesForCoordinate(Puzzle puzzle, int x, int y)
+        {
+            var result = new List<int>();
+
+            var rowValues = puzzle.Array[x];
+            var columnValues = new int?[9]
+            {
+                puzzle.Array[0][y],
+                puzzle.Array[1][y],
+                puzzle.Array[2][y],
+                puzzle.Array[3][y],
+                puzzle.Array[4][y],
+                puzzle.Array[5][y],
+                puzzle.Array[6][y],
+                puzzle.Array[7][y],
+                puzzle.Array[8][y]
+            };
+            var boxRows = y > 5 ? new int[3] { 6, 7, 8 } : y > 2 ? new int[3] { 3, 4, 5 } : new int[3] { 0, 1, 2 };
+            var boxColumns = x > 5 ? new int[3] { 6, 7, 8 } : x > 2 ? new int[3] { 3, 4, 5 } : new int[3] { 0, 1, 2 };
+            var BoxValues = new int?[9]
+            {
+                puzzle.Array[boxColumns[0]][boxRows[0]],
+                puzzle.Array[boxColumns[0]][boxRows[1]],
+                puzzle.Array[boxColumns[0]][boxRows[2]],
+                puzzle.Array[boxColumns[1]][boxRows[0]],
+                puzzle.Array[boxColumns[1]][boxRows[1]],
+                puzzle.Array[boxColumns[1]][boxRows[2]],
+                puzzle.Array[boxColumns[2]][boxRows[0]],
+                puzzle.Array[boxColumns[2]][boxRows[1]],
+                puzzle.Array[boxColumns[2]][boxRows[2]]
+
+                //puzzle.Array[boxRows[0]][boxColumns[0]],
+                //puzzle.Array[boxRows[0]][boxColumns[1]],
+                //puzzle.Array[boxRows[0]][boxColumns[2]],
+                //puzzle.Array[boxRows[1]][boxColumns[0]],
+                //puzzle.Array[boxRows[1]][boxColumns[1]],
+                //puzzle.Array[boxRows[1]][boxColumns[2]],
+                //puzzle.Array[boxRows[2]][boxColumns[0]],
+                //puzzle.Array[boxRows[2]][boxColumns[1]],
+                //puzzle.Array[boxRows[2]][boxColumns[2]]
+            };
+
+            for (int i = 1; i <= 9; i++)
+            {
+                if (!(Array.IndexOf(rowValues, i) > -1) && !(Array.IndexOf(columnValues, i) > -1) && !(Array.IndexOf(BoxValues, i) > -1))
+                {
+                    result.Add(i);
+                }
+            }
+
+            // TODO: Do something
+            return result;
+        }
+        #endregion
+    }
+}
